820596d4b3988f69d514d9b30992c4d55f263d9c
Michael Stummvoll <michael.stummvoll@autinity.de>
2015-06-11 10:48:43 +0200

    Tests for expressions with variables

--- src/test/java/org/stummi/evaluator/test/EvaluatorTests.java
+++ src/test/java/org/stummi/evaluator/test/EvaluatorTests.java
@@ -1,7 +1,11 @@
 package org.stummi.evaluator.test;
 
+import java.util.Collections;
+import java.util.function.DoubleUnaryOperator;
+
 import org.junit.Assert;
 import org.junit.Test;
+import org.stummi.evaluator.Expression;
 import org.stummi.evaluator.SimpleEvaluator;
 import org.stummi.evaluator.exception.EvaluatorException;
 
@@ -28,13 +32,30 @@ public class EvaluatorTests {
 		testStaticExpression("5 - - - 3", 5 - - -3);
 	}
 	
+	@Test
 	public void testPrecedences() throws EvaluatorException {
 		testStaticExpression("2 * 3 + 4", 2 * 3 + 4);
 		testStaticExpression("2 + 3 * 4", 2 + 3 * 4);
 		testStaticExpression("(2+3) * 4", (2 + 3) * 4);
 		testStaticExpression("-(2+3) * (4+5)", -(2 + 3) * (4 + 5));
 	}
+	
+	@Test
+	public void testVariables() throws EvaluatorException {
+		testExpressionWithX("x", x -> x);
+		testExpressionWithX("x * 2", x -> x*2);
+		testExpressionWithX("x + 2", x -> x+2);
+		testExpressionWithX("2*(x+1)", x -> 2*(x+1));
+		testExpressionWithX("2*-x", x -> 2*-x);
+	}
 
+	private void testExpressionWithX(String expression, DoubleUnaryOperator result) throws EvaluatorException {
+		Expression exp = evaluator.parseExpression(expression);
+		for(double x = 0; x<100; x+=0.1) {
+			Assert.assertEquals(result.applyAsDouble(x), exp.run(Collections.singletonMap("x", x)), 1e-8);
+		}
+	}
+	
 	private void testStaticExpression(String expression, double result) throws EvaluatorException {
 		Assert.assertEquals(result, evaluator.evaluate(expression), 1e-8);
 	}
