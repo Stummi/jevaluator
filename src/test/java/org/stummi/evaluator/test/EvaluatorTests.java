56f0f020cc90e8c4c7e07a8162f3bec89e8b88ab
Michael Stummvoll <michael.stummvoll@autinity.de>
2015-06-11 10:33:05 +0200

    more tests

--- src/test/java/org/stummi/evaluator/test/EvaluatorTests.java
+++ src/test/java/org/stummi/evaluator/test/EvaluatorTests.java
@@ -14,16 +14,25 @@ public class EvaluatorTests {
 	}
 
 	@Test
-	public void testSimpleExpressions() throws EvaluatorException {
+	public void testSimpleConstantExpressions() throws EvaluatorException {
 		testStaticExpression("2", 2);
 		testStaticExpression("2+3", 2 + 3);
 		testStaticExpression("10/2", 10 / 2);
 		testStaticExpression("10/3", 10 / 3D);
 		testStaticExpression("5-3", 5 - 3);
+		testStaticExpression("5 - + 3", 5 - +3);
 		testStaticExpression("5 + - 3", 5 + -3);
+		testStaticExpression("5 + - + - + - + - + - + + + - - - + + - - + - 3", 5 + - + - + - + - + - + + + - - - + + - - + - 3);
 		testStaticExpression("5 - - 3", 5 - -3);
 		testStaticExpression("5 - - 3", 5 - -3);
-		testStaticExpression("5 - - - 3", 5 - - - 3);
+		testStaticExpression("5 - - - 3", 5 - - -3);
+	}
+	
+	public void testPrecedences() throws EvaluatorException {
+		testStaticExpression("2 * 3 + 4", 2 * 3 + 4);
+		testStaticExpression("2 + 3 * 4", 2 + 3 * 4);
+		testStaticExpression("(2+3) * 4", (2 + 3) * 4);
+		testStaticExpression("-(2+3) * (4+5)", -(2 + 3) * (4 + 5));
 	}
 
 	private void testStaticExpression(String expression, double result) throws EvaluatorException {
